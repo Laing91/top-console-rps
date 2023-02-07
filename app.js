735d24d3511887a8f2db6a64ce7ec69d136bdd85
Laing91 <86000107+Laing91@users.noreply.github.com>
2023-02-07 21:11:50 +0000

    Added playRound function

--- app.js
+++ app.js
@@ -12,5 +12,34 @@ let getComputerChoice = () => {
     } else {
         return "Scissors";
     }
-    console.log(randomNumber);
 }
+
+// Task 2 - Write a function that plays a single round of Rock, Paper, cissors. The function
+// should take two parameters - playerSelection and computerSelection, and return
+// a string that declares the winner of the round like so "You lose! Paper beats Rock"
+
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "It/'s a draw, you both selected Rock."
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You lose! Paper beats Rock."
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors."
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock."
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return "It/'s a draw, you both selected Paper."
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beat Paper."
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors."
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beat Paper."
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        return "It/'s a draw, you both selected Rock."
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
